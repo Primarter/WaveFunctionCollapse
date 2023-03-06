e5e4d75526666b3ad71f57e0a37ac06627fb89ce
Camille Ferrari <primarter@gmail.com>
2023-03-06 13:23:12 +0000

    started face profile name construction and added face profile 2d proj

--- Test.py
+++ Test.py
@@ -1,6 +1,7 @@
 import bpy
 from pathlib import Path
 from typing import List, Tuple, Dict
+from collections import Counter
 
 
 '''
@@ -8,34 +9,56 @@ hash(bpy.context.object.location.xyz.freeze())
 bpy.data.collections["Prototypes"].all_objects[0].data.vertices[0].co
 '''
 
-PATH = Path('D:/Github/BlenderScripting/test.txt')
+PATH = Path('./test.txt')
 
 objects = bpy.data.collections['Prototypes'].all_objects
 
 file = open(PATH, 'w')
 
+known_face_profiles: List[Tuple[Tuple, str]] = []
+
 for obj in objects:
 
     points = [v.co[:] for v in obj.data.vertices] 
 
-    comparisons = (
-        lambda p: p[0] == -1, # nx
-        lambda p: p[0] == +1, # px
-        lambda p: p[1] == -1, # ny
-        lambda p: p[1] == +1, # py
-        lambda p: p[2] == -1, # nz
-        lambda p: p[2] == +1, # pz
-    )
-
     tuple_points: List[Tuple] = [point[:] for point in points] # converts blender Vector to tuple to avoid using freeze
 
-    face_profiles: Tuple[List] = ([], [], [], [], [], [])
+    face_profiles_geometry: Tuple[List] = ([], [], [], [], [], [])
+
+    face_profiles_names: Tuple[str] = ('', '', '', '', '', '')
 
     for p in tuple_points:
-        for i, comp in enumerate(comparisons):
-            if comp(p):
-                face_profiles[i].append(p)
+        if (p[0] == -1): # nx
+            point_2d_proj = (-p[1], p[2])
+            face_profiles_geometry[0].append(point_2d_proj)
+        if (p[0] == +1): # px
+            point_2d_proj = (p[1], p[2])
+            face_profiles_geometry[1].append(point_2d_proj)
+        if (p[1] == -1): # ny
+            point_2d_proj = (p[0], p[2])
+            face_profiles_geometry[2].append(point_2d_proj)
+        if (p[1] == +1): # py
+            point_2d_proj = (-p[0], p[2])
+            face_profiles_geometry[3].append(point_2d_proj)
+        if (p[2] == -1): # nz
+            point_2d_proj = (p[0], -p[1])
+            face_profiles_geometry[4].append(point_2d_proj)
+        if (p[2] == +1): # pz
+            point_2d_proj = (p[0], p[1])
+            face_profiles_geometry[5].append(point_2d_proj)
 
-    file.write(str(face_profiles) + '\n')
+    for i, fpg in enumerate(face_profiles_geometry):
+        for (kfp, name) in known_face_profiles:
+            if (Counter(kfp) == Counter(fpg)):
+                face_profiles_names[i] = name
+                break
+        else:
+            # todo: add check for symmetry and construct name for face profile
+            pass
+
+    # for vfp in vertices_face_profiles:
+        
+
+    # file.write(str(vertices_face_profiles) + '\n')
 
 file.close()
