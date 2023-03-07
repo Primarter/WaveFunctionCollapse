b84f981aa99a7da11bc96bb113448706b47cc867
Camille Ferrari <primarter@gmail.com>
2023-03-07 19:34:39 +0000

    Face profiles generated, normals normalised after projection, outputted to a file next to blender exec bc perms are shit

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@ import bpy
 from pathlib import Path
 from typing import List, Tuple, Dict
 from collections import Counter
-
+from mathutils import Vector
 
 '''
 hash(bpy.context.object.location.xyz.freeze())
@@ -15,7 +15,8 @@ objects = bpy.data.collections['Prototypes'].all_objects
 
 file = open(PATH, 'w')
 
-known_face_profiles: List[Tuple[Tuple, str]] = []
+known_face_profiles: List[Tuple[Tuple, str]] = [([], '-1')]
+known_vertical_face_profiles: List[Tuple[Tuple, str]] = [([], '-1')]
 
 for obj in objects:
 
@@ -28,26 +29,32 @@ for obj in objects:
     face_profiles_geometry: Tuple[List] = ([], [], [], [])
     vertical_face_profiles_geometry: Tuple[List] = ([], [])
 
-    face_profiles_names: Tuple[str] = ('', '', '', '', '', '')
+    face_profiles_names: List[str] = ['', '', '', '', '', '']
 
     for i, p in enumerate(tuple_points):
         if (p[0] == -1): # nx
-            point_2d_proj = (-p[1], p[2], -tuple_normals[i][1], tuple_normals[i][2])
+            projNormals = Vector((-tuple_normals[i][1], tuple_normals[i][2], 0)).normalized()[:2]
+            point_2d_proj = (-p[1], p[2], *projNormals)
             face_profiles_geometry[0].append(point_2d_proj)
         if (p[0] == +1): # px
-            point_2d_proj = (p[1], p[2], tuple_normals[i][1], tuple_normals[i][2])
+            projNormals = Vector((tuple_normals[i][1], tuple_normals[i][2], 0)).normalized()[:2]
+            point_2d_proj = (p[1], p[2], *projNormals)
             face_profiles_geometry[1].append(point_2d_proj)
         if (p[1] == -1): # ny
-            point_2d_proj = (p[0], p[2], tuple_normals[i][0], tuple_normals[i][2])
+            projNormals = Vector((tuple_normals[i][0], tuple_normals[i][2], 0)).normalized()[:2]
+            point_2d_proj = (p[0], p[2], *projNormals)
             face_profiles_geometry[2].append(point_2d_proj)
         if (p[1] == +1): # py
-            point_2d_proj = (-p[0], p[2], -tuple_normals[i][0], tuple_normals[i][2])
+            projNormals = Vector((-tuple_normals[i][0], tuple_normals[i][2], 0)).normalized()[:2]
+            point_2d_proj = (-p[0], p[2], *projNormals)
             face_profiles_geometry[3].append(point_2d_proj)
         if (p[2] == -1): # nz
-            point_2d_proj = (p[0], -p[1], tuple_normals[i][0], -tuple_normals[i][1])
+            projNormals = Vector((tuple_normals[i][0], -tuple_normals[i][1], 0)).normalized()[:2]
+            point_2d_proj = (p[0], -p[1], *projNormals)
             vertical_face_profiles_geometry[0].append(point_2d_proj)
         if (p[2] == +1): # pz
-            point_2d_proj = (p[0], p[1], tuple_normals[i][0], tuple_normals[i][1])
+            projNormals = Vector((tuple_normals[i][0], tuple_normals[i][1], 0)).normalized()[:2]
+            point_2d_proj = (p[0], p[1], *projNormals)
             vertical_face_profiles_geometry[1].append(point_2d_proj)
 
     for i, fpg in enumerate(face_profiles_geometry):
@@ -56,12 +63,40 @@ for obj in objects:
                 face_profiles_names[i] = name
                 break
         else:
-            # todo: this is where you create the different profiles if they don't exist yet
-            # if i == 4 or 5, then vertical, otherwise normal.
-            # remember to flip both points and normals to check
-            # if flipped and unflipped are equal, name == len(known_profiles) + s and store one
-            # if flipping changes, save both with f and without f
-            pass
+            flipped_fpg = [(-x, y, -nx, ny) for (x, y, nx, ny) in fpg]
+            if Counter(fpg) == Counter(flipped_fpg):
+                name = str(len(known_face_profiles)) + 's'
+                known_face_profiles.append((fpg, name))
+            else:
+                name = str(len(known_face_profiles))
+                known_face_profiles.append((fpg, name))
+                name += 'f'
+                known_face_profiles.append((flipped_fpg, name))
+
+    for i, fpg in enumerate(vertical_face_profiles_geometry):
+        for (kfp, name) in known_vertical_face_profiles:
+            if (Counter(kfp) == Counter(fpg)):
+                face_profiles_names[i] = name
+                break
+        else:
+            rot_1 = [(-x, y, -nx, ny) for (x, y, nx, ny) in fpg]
+            rot_2 = [(-x, -y, -nx, -ny) for (x, y, nx, ny) in fpg]
+            rot_3 = [(x, -y, nx, -ny) for (x, y, nx, ny) in fpg]
+            name = 'v' + str(len(known_vertical_face_profiles)) + '_'
+            if (Counter(fpg) == Counter(rot_1) and Counter(fpg) == Counter(rot_2) and Counter(fpg) == Counter(rot_3)):
+                known_vertical_face_profiles.append((fpg, name + 'i'))
+            else:
+                known_vertical_face_profiles.append((fpg, name + '0'))
+                known_vertical_face_profiles.append((rot_1, name + '1'))
+                known_vertical_face_profiles.append((rot_2, name + '2'))
+                known_vertical_face_profiles.append((rot_3, name + '3'))
+
+for fp, name in known_face_profiles:
+    file.write(str(fp) + ' ' + name + '\n')
+
+for fp, name in known_vertical_face_profiles:
+    file.write(str(fp) + ' ' + name + '\n')
+
 
 '''
 as a rule for placement, overlapping geometry shouldn't be authorized
